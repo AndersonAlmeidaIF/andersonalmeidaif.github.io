424f0c6556091836edec20d16d03dc5485a5c77d
Anderson Almeida <anderson.almeida@ifsp.edu.br>
2025-03-12 18:48:06 -0300

    deploy itparcp/aula01

---
+++ itparcp/aula01/assets/md-DKt5wBGx.js
@@ -0,0 +1,6 @@
+import{o as s,c as l,k as n,e,q as i,s as p,B as a}from"./modules/vue-DRX4njMG.js";import{a as u}from"./default-C8NagOJ7.js";import{u as c,f as m}from"./slidev/context-CojKsDQl.js";import"./index-DXC1LSyM.js";import"./modules/shiki-1EDlywvT.js";const b={__name:"aula01.md__slidev_13",setup(d){const{$slidev:f,$nav:_,$clicksContext:r,$clicks:g,$page:$,$renderContext:x,$frontmatter:t}=c();return r.setup(),(C,o)=>(s(),l(u,i(p(a(m)(a(t),12))),{default:n(()=>o[0]||(o[0]=[e("h1",null,"Conteúdo Programático",-1),e("hr",null,null,-1),e("p",null,"● Lógica Digital:",-1),e("pre",null,[e("code",null,`  - Álgebra booleana;
+  - Portas Lógicas e Circuitos Lógicos;
+  - Multiplexadores;
+  - Somadores;
+  - Flip-flops;
+`)],-1),e("p",null,"● Registradores;",-1)])),_:1},16))}};export{b as default};
